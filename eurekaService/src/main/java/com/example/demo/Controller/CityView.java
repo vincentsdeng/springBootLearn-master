a38ca57b7b447b9b00186ecd1646702cb1db5832
vincentsdeng <tdeng377@gmail.com>
2018-12-21 15:58:00 +0800

    Modify annotations
    restController

--- eurekaService/src/main/java/com/example/demo/Controller/CityView.java
+++ eurekaService/src/main/java/com/example/demo/Controller/CityView.java
@@ -1,8 +1,10 @@
 package com.example.demo.Controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RestController;
 
 
-@Controller
+@RestController
 public class CityView {
+
 }
